78775144ac0ba06b2d505230a2eb43e1199c487a
Harrisonp365 <harrisonpowell365@gmail.com>
2020-12-17 16:11:15 +1100

    versions have been added, complete https server tomorrow, basic loops for matching characters compelte and currently hardcoded in

--- UnderstandBruteforceAttack/UnderstandBruteforceAttack/src/Main.cpp
+++ UnderstandBruteforceAttack/UnderstandBruteforceAttack/src/Main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-char alpha[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0' };
+char alpha[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 int main()
 {
-	string userpass = "ab";
+	string userpass = "abc abc";
 
+
+	/* // Initial version
+ 	size_t passIndex = 0;
 	for (int i = 0; i < sizeof(alpha); i++)
 	{
-		cout << alpha[i];
+		if (passIndex >= 3)
+			return 0;
+
+		if (alpha[i] == userpass[passIndex])
+		{
+			cout << alpha[i];
+			passIndex++;
+			i = -1;
+		}
+		cout << "index #: " << passIndex << endl;
+
+	}*/
+	
+
+	for (int i = 0; i < userpass.length(); i++)
+	{
+		for (int j = 0; j < sizeof(alpha); j++)
+		{
+			if (alpha[j] == userpass[i])
+			{
+				cout << alpha[j];
+			}
+		}
 	}
 };
