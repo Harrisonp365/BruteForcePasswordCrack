7819ff7089527d5276cc44be1b5430a26ab2e206
Harrisonp365 <harrisonpowell365@gmail.com>
2020-12-18 14:25:28 +1100

    Sorting individual characters, numbers and symbols works and is fast. Next is to try to match full words as a whole, for proper brute force attacks

--- UnderstandBruteforceAttack/UnderstandBruteforceAttack/src/Main.cpp
+++ UnderstandBruteforceAttack/UnderstandBruteforceAttack/src/Main.cpp
@@ -2,11 +2,13 @@
 #include <string>
 using namespace std;
 
-char alpha[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+char alpha[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+				'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+				'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '|' };
 
 int main()
 {
-	string userpass = "abc abc";
+	string userpass = "aAbB";
 
 
 	/* // Initial version
@@ -24,9 +26,9 @@ int main()
 		}
 		cout << "index #: " << passIndex << endl;
 
-	}*/
-	
-
+	}*/	
+	/*
+	//Second version to match lowercase, uppercase, numbers and symbols individually
 	for (int i = 0; i < userpass.length(); i++)
 	{
 		for (int j = 0; j < sizeof(alpha); j++)
@@ -34,6 +36,18 @@ int main()
 			if (alpha[j] == userpass[i])
 			{
 				cout << alpha[j];
+			}
+		}
+	}
+	*/
+	//This version must match 2 characters at a time rather than one
+	for (int i = 0; i < userpass.length(); i++)
+	{
+		for (int j = 0; j < sizeof(alpha); j++)
+		{
+			if (alpha[j + (j+1)] == userpass[i + (i+1)])
+			{
+				cout << alpha[j+(j++)]; // This causes a crash after matching 2 of the same characters.. eg(abAB) this crashes...(debug assertion failed)
 			}
 		}
 	}
